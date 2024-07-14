ab3fa04a82ae9c708c4ccef9c396f84ccc71cbd6
Gabriel Kruger <gabrielkr32@gmail.com>
2024-07-14 08:25:53 -0300

    definido regras para a Logica da Escala

---
+++ logicaEscala.py
@@ -0,0 +1,10 @@
+#Definir os Trabalhadores e os Dias da Semana:
+
+#Temos 11 trabalhadores.
+#A semana tem 7 dias.
+#Determinar Quantos Trabalhadores por Dia:
+
+#Cada dia da semana (segunda a domingo) deve ter entre 7 e 9 trabalhadores.
+#Atribuir Folgas:
+
+#A cada ciclo de 5 dias (uma semana), 2 ou 3 trabalhadores devem tirar folga.
